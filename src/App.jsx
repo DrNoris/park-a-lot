81acbab40a3696247922f96b0ee1e880515567ea
DrNoris <noris.aurelian.net@gmail.com>
2025-03-20 16:02:36 +0200

    modified:   src/App.jsx 	modified:   vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register'; 
 import Home from './pages/Home'
 import Login from './pages/Login'
