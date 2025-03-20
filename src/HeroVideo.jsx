03e967fea221c5738d94f91d8d114b8157ada064
DrNoris <noris.aurelian.net@gmail.com>
2025-03-20 16:35:20 +0200

    modified:   src/HeroVideo.jsx

--- src/HeroVideo.jsx
+++ src/HeroVideo.jsx
@@ -9,7 +9,7 @@ function HeroVideo() {
         </div>
     
         <div class="relative z-10 flex flex-col justify-center items-center h-full text-center">
-            <h1 class="text-5xl font-bold leading-tight mb-4">Welcome to Park a Lort</h1>
+            <h1 class="text-5xl font-bold leading-tight mb-4">Welcome to Park a Lot</h1>
             <p class="text-lg text-gray-300 mb-8">Parking made easier</p>
             <a href="#" class="bg-transparent border border-white text-white hover:bg-galben hover:border-transparent py-2 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">Get Started</a>
         </div>
